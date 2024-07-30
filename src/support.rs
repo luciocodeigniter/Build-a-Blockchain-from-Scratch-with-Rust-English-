551c338847a76113536a6893b61903c27ac2c7fd
Lucio <luciocodeigniter@gmail.com>
2024-07-30 20:30:34 -0300

    melhorando o comentário

--- src/support.rs
+++ src/support.rs
@@ -1,36 +1,42 @@
-/// the most primitive representation of a block in our blockchain
+/// A representação mais básica de um bloco em nossa blockchain
 pub struct Block<Header, Extrinsic> {
-    /// the block header contains metadata about the block
+    /// O cabeçalho do bloco contém metadados sobre o bloco, como número e hash
     pub header: Header,
 
-    /// the extrinsic represents the state transitions to be executed in this block
+    /// As extrinsics representam as transações ou mudanças de estado a serem executadas neste bloco
     pub extrinsic: Vec<Extrinsic>,
 }
 
+/// Estrutura que representa o cabeçalho de um bloco
+/// Contém informações essenciais sobre o bloco
 pub struct Header<BlockNumber> {
+    /// O número do bloco, que indica sua posição na cadeia
     pub block_number: BlockNumber,
 }
 
-/// this is as 'extrinsic': literaly an a external message from
-/// outside of blockchain. This simplified version of an extrinsic tell us
-/// who is making the call and wich call they are making
+/// Isto é uma 'extrinsic': uma mensagem externa que vem de fora da blockchain.
+/// Esta versão simplificada de uma extrinsic nos informa quem está fazendo a chamada
+/// e qual chamada está sendo feita
 pub struct Extrinsic<Caller, Call> {
+    /// O endereço ou identificador de quem está fazendo a chamada
     pub caller: Caller,
+    /// A ação ou função que está sendo chamada
     pub call: Call,
 }
 
-/// The Result type of our runtime, when everything completes successfuly, we return 'Ok(())',
-/// otherwise, return a static error message
+/// O tipo de resultado do nosso runtime. Quando tudo é concluído com sucesso,
+/// retornamos 'Ok(())', caso contrário, retornamos uma mensagem de erro estática
 pub type DispachResult = Result<(), &'static str>;
 
 pub trait Dispatch {
-    /// the type used to identify the caller of the function
+    /// O tipo usado para identificar quem está fazendo a chamada
     type Caller;
 
-    /// the state transition function call the caller is trying to access
+    /// O tipo que representa a função de transição de estado que o chamador está tentando acessar
     type Call;
 
-    /// This function takes a 'caller' and the 'call' they want to make,
-    /// and returns a 'Result' based on the outcome of that function call
+    /// Esta função recebe um 'caller' e a 'call' que ele quer fazer,
+    /// e retorna um 'Result' baseado no resultado dessa chamada de função.
+    /// Ela é responsável por executar a lógica da transação.
     fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> DispachResult;
 }
